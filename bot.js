89b39f76b43310bdb2bc375cde956fa6098038c4
Your Name <you@example.com>
2026-07-14 21:28:49 -0600

    save

--- bot.js
+++ bot.js
@@ -3,11 +3,14 @@ import { CoolColors } from "./colors.js";
 export class Bot {
     constructor(x, y) {
         this.position = { x:x, y:y };
+        this.radius = 20;
     }
 
     render(ctx) {
         ctx.fillStyle = CoolColors.MAGENTA;
-        ctx.fillRect(this.position.x, this.position.y, 50, 50);
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
     }
 
     update(deltaTime) {
